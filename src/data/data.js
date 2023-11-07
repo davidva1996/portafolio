3f0bbcb7de646974bf7238c9299c9d28bd7b4e70
David Vanegas <david-9225@hotmail.com>
2023-11-07 00:08:13 -0500

    Agregue mas paginas a mi portafolio

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,9 @@ import Viajes from '../assets/projects/Viajes.png'
 import Finanzas from '../assets/projects/analisis_datos.png'
 import Restaurante from '../assets/projects/Restaurante.png'
 import Juego from '../assets/projects/Juego.png'
-import Tareas from '../assets/projects/tareas_app.png'
+import Auto from '../assets/projects/auto.png'
+import Banco from '../assets/projects/banco.png'
+import Clima from '../assets/projects/clima.png'
 
 export const data=[
     {
@@ -36,11 +38,27 @@ export const data=[
 
     {
         id:5,
-        name:"App de tareas",
-        image:Tareas,
+        name:"Renta de autos",
+        image:Auto,
         github:"",
-        live:"https://davidva1996.github.io/crud/build",
+        live:"https://davidva1996.github.io/carros/build",
     },
+    {
+        id:6,
+        name:"Credito hipotecario",
+        image:Banco,
+        github:"",
+        live:"https://davidva1996.github.io/credito/",
+    },
+
+    {
+        id:7,
+        name:"Clima App",
+        image:Clima,
+        github:"",
+        live:"https://davidva1996.github.io/weather-app/build",
+    },
+
 
 
 ]
